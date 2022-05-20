ad8ca557ce63fe4c751d37be300559c3d8b0f478
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-05-20 20:09:47 +0000

    Sign in

---
+++ lib/Screens/Authenticate/signin.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Singin extends StatefulWidget {
+  const Singin({Key? key}) : super(key: key);
+
+  @override
+  State<Singin> createState() => _SinginState();
+}
+
+class _SinginState extends State<Singin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}

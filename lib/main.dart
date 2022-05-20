3302f35df7603ee03282cc6e847021323290e684
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-05-20 19:19:42 +0000

    Initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fire/Screens/Wrapper.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Wrapper(),
        );
   }
 }

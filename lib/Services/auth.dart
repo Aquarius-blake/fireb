b3f8e2b7161365cd9b4e4c3a9097d522580fd67f
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-05-20 19:57:14 +0000

    Initial

---
+++ lib/Services/auth.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+
+  final FirebaseAuth _auth=FirebaseAuth.instance;
+
+  //sign in as guest
+
+
+  //sign in with email
+
+
+  //signout
+
+}

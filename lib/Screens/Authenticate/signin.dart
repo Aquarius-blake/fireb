572c87112c2465e0f73ef92fcee934bd67de0fa8
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-05-21 12:47:16 +0000

    Sign in

--- lib/Screens/Authenticate/signin.dart
+++ lib/Screens/Authenticate/signin.dart
@@ -78,7 +78,7 @@ Padding(
                         },
 
                         child: Padding(
-                          padding: const EdgeInsets.fromLTRB(100.0,0,100.0,0),
+                          padding: const EdgeInsets.fromLTRB(80.0,0,80.0,0),
                           child: Text(
                             "Sign in as Guest",
 

1bc9c68241800822fe2eb20b7cf54a0e338275f1
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-05-30 20:00:20 +0000

    Wrapper Provider set up

--- lib/Screens/Wrapper.dart
+++ lib/Screens/Wrapper.dart
@@ -1,13 +1,16 @@
 import 'package:fire/Screens/Authenticate/authenticate.dart';
 import 'package:fire/Screens/Home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../Models/Users1.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
+final user =Provider.of<User1?>(context);
     //todo:return home or authenticate
     return Authenticate();
   }

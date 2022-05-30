c88c7da424964a96ac76c3999aef4f22644be678
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-05-30 19:55:06 +0000

    provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:fire/Screens/Wrapper.dart';
+import 'package:fire/Services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,9 +16,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
-       );
+    return StreamProvider.value(
+      value: AuthService().user,
+      initialData: null,
+      child: MaterialApp(
+        home: Wrapper(),
+         ),
+    );
   }
 }
 

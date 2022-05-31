9b2b68056d1058e810617c975461cb9b51346616
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-05-31 19:47:56 +0000

    Drawer

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -40,7 +40,7 @@ class _HomeState extends State<Home> {
                     child: Column(
                       children: <Widget>[
                         CircleAvatar(
-radius: 40.0,
+radius: 60.0,
                         ),
 
                       ],

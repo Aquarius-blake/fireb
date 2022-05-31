5de5fa280a13eba51bbd430a7d33f1021c884d2a
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-05-31 19:42:53 +0000

    Home to stateful widget

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -36,48 +36,50 @@ class _HomeState extends State<Home> {
           child: Column(
             children: <Widget>[
               DrawerHeader(
-                  child: Column(
-                    children: <Widget>[
-                      CircleAvatar(
-
-                      ),
-                      SizedBox(height: 10,),
-                      ListTile(
-                        leading: Icon(Icons.person,
-                          color: Colors.black, size:50.0,),
-                        title: Text(
-                          "Profile",
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                            fontStyle: FontStyle.italic,
-
-                          ),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: <Widget>[
+                        CircleAvatar(
 
                         ),
-                        onTap: () async{
+                        SizedBox(height: 10,),
+                        ListTile(
+                          leading: Icon(Icons.person,
+                            color: Colors.black, size:50.0,),
+                          title: Text(
+                            "Profile",
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic,
 
-                        },
-                      ),
-                      SizedBox(height: 40,),
-                      ListTile(
-                        leading: Icon(LineIcons.alternateSignOut,
-                          color: Colors.black, size:50.0,),
-                        title: Text(
-                          "Sign Out",
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                            fontStyle: FontStyle.italic,
+                            ),
 
                           ),
+                          onTap: () async{
 
+                          },
                         ),
-                        onTap: () async{
-                          await _auth.SignOut();
-                        },
-                      ),
-                    ],
+                        SizedBox(height: 40,),
+                        ListTile(
+                          leading: Icon(LineIcons.alternateSignOut,
+                            color: Colors.black, size:50.0,),
+                          title: Text(
+                            "Sign Out",
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic,
+
+                            ),
+
+                          ),
+                          onTap: () async{
+                            await _auth.SignOut();
+                          },
+                        ),
+                      ],
+                    ),
                   ))
             ],
           ),

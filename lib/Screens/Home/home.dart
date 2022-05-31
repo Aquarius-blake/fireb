3669fbe2f8d9ef863c3dfb68ff527a971d0ef8b1
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-05-31 19:40:54 +0000

    Home to stateful widget

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -59,7 +59,7 @@ class _HomeState extends State<Home> {
 
                         },
                       ),
-                      SizedBox(height: 10,),
+                      SizedBox(height: 20,),
                       ListTile(
                         leading: Icon(LineIcons.alternateSignOut,
                           color: Colors.black, size:50.0,),

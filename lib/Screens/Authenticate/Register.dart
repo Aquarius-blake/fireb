2edb0ccbd4647834c4161cf13f36ddd99175c32f
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-03 13:55:49 +0000

    register

--- lib/Screens/Authenticate/Register.dart
+++ lib/Screens/Authenticate/Register.dart
@@ -37,172 +37,175 @@ class _RegisterState extends State<Register> {
         padding: const EdgeInsets.all(18.0),
         child: Container(
           margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 10.0),
-          child: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Container(
 color: Colors.lightBlueAccent[100],
-            child: SingleChildScrollView(
-              child: Form(
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height:20 ,),
-                    Icon(
-                      Icons.forum,
-                      color: Colors.white70,
-                      size: 90,
-                    ),
-                    SizedBox(height:10 ,),
-                    TextFormField(
-                      onChanged: (val){
-                        setState(() {
-                          Fnmae=val;
-                        });
-                      },
-                      decoration: InputDecoration(
-                        hintText: "Enter Full Name",
-                        filled: true,
-                        fillColor: Colors.white70,
+              child: SingleChildScrollView(
+                child: Form(
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height:20 ,),
+                      Icon(
+                        Icons.forum,
+                        color: Colors.white70,
+                        size: 90,
                       ),
+                      SizedBox(height:10 ,),
+                      TextFormField(
+                        onChanged: (val){
+                          setState(() {
+                            Fnmae=val;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Enter Full Name",
+                          filled: true,
+                          fillColor: Colors.white70,
+                        ),
 
-                      style: TextStyle(
+                        style: TextStyle(
 
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20,),
-                    TextFormField(
-                      onChanged: (val){
-                        setState(() {
-                          Username=val;
-                        });
-                      },
-                      decoration: InputDecoration(
-                        hintText: "Enter Username",
-                        filled: true,
-                        fillColor: Colors.white70,
+                      SizedBox(height: 20,),
+                      TextFormField(
+                        onChanged: (val){
+                          setState(() {
+                            Username=val;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Enter Username",
+                          filled: true,
+                          fillColor: Colors.white70,
+                        ),
+
+                        style: TextStyle(
+
+                        ),
                       ),
+                      SizedBox(height: 20,),
+                      TextFormField(
+                        onChanged: (val){
+                          setState(() {
+                            email=val;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          hintText: "Enter Email Address",
+                          filled: true,
+                          fillColor: Colors.white70,
+                        ),
 
-                      style: TextStyle(
+                        style: TextStyle(
 
+                        ),
                       ),
-                    ),
-                    SizedBox(height: 20,),
-                    TextFormField(
-                      onChanged: (val){
-                        setState(() {
-                          email=val;
-                        });
-                      },
-                      decoration: InputDecoration(
-                        hintText: "Enter Email Address",
-                        filled: true,
-                        fillColor: Colors.white70,
+                      SizedBox(height: 20,),
+                      TextFormField(
+                        onChanged: (val){
+                          setState(() {
+                            password=val;
+                          });
+                        },
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: "Enter Password",
+                          filled: true,
+                          fillColor: Colors.white70,
+                        ),
+                        style: TextStyle(
+
+                        ),
                       ),
+                      SizedBox(height: 50,),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
 
-                      style: TextStyle(
-
-                      ),
-                    ),
-                    SizedBox(height: 20,),
-                    TextFormField(
-                      onChanged: (val){
-                        setState(() {
-                          password=val;
-                        });
-                      },
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        hintText: "Enter Password",
-                        filled: true,
-                        fillColor: Colors.white70,
-                      ),
-                      style: TextStyle(
-
-                      ),
-                    ),
-                    SizedBox(height: 50,),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-
-                      children: <Widget>[
-                        Center(
-                          child: ElevatedButton(
-                              style: ButtonStyle(
-
-                              ),
-                              onPressed: () async {
-                                print(email);
-                                print(password);
-
-                              },
-
-                              child: Padding(
-                                padding: const EdgeInsets.fromLTRB(100.0,0.0,100.0,0),
-                                child: Text(
-                                  "Sign in ",
-
+                        children: <Widget>[
+                          Center(
+                            child: ElevatedButton(
+                                style: ButtonStyle(
 
                                 ),
-                              )
-                          ),
-                        ),
-                        Center(
-                          child: ElevatedButton(
-                              style: ButtonStyle(
+                                onPressed: () async {
+                                  print(email);
+                                  print(password);
 
-                              ),
-                              onPressed: () async {
-                                dynamic result= await _auth.Signguest();
-                                if(result== null){
-                                  print("Error Signing into App");
-                                }else
-                                {
-                                  print("Success");
-                                  print(result.UID);
-                                }
+                                },
 
-                              },
+                                child: Padding(
+                                  padding: const EdgeInsets.fromLTRB(100.0,0.0,100.0,0),
+                                  child: Text(
+                                    "Sign in ",
 
-                              child: Padding(
-                                padding: const EdgeInsets.fromLTRB(75.0,0,75.0,0),
-                                child: Text(
-                                  "Sign in as Guest",
-                                  style: TextStyle(
 
                                   ),
+                                )
+                            ),
+                          ),
+                          Center(
+                            child: ElevatedButton(
+                                style: ButtonStyle(
 
                                 ),
-                              )
+                                onPressed: () async {
+                                  dynamic result= await _auth.Signguest();
+                                  if(result== null){
+                                    print("Error Signing into App");
+                                  }else
+                                  {
+                                    print("Success");
+                                    print(result.UID);
+                                  }
+
+                                },
+
+                                child: Padding(
+                                  padding: const EdgeInsets.fromLTRB(75.0,0,75.0,0),
+                                  child: Text(
+                                    "Sign in as Guest",
+                                    style: TextStyle(
+
+                                    ),
+
+                                  ),
+                                )
+                            ),
                           ),
-                        ),
-                        Center(
-                          child: ElevatedButton(
-                              style: ButtonStyle(
-
-                              ),
-                              onPressed: () async {
-                                dynamic result= await _auth.Signguest();
-                                if(result== null){
-                                  print("Error ");
-                                }else
-                                {
-                                  print("Success");
-                                }
-
-                              },
-
-                              child: Padding(
-                                padding: const EdgeInsets.fromLTRB(100.0,0,100.0,0),
-                                child: Text(
-                                  "Register",
-
-
+                          Center(
+                            child: ElevatedButton(
+                                style: ButtonStyle(
 
                                 ),
-                              )
-                          ),
-                        )
+                                onPressed: () async {
+                                  dynamic result= await _auth.Signguest();
+                                  if(result== null){
+                                    print("Error ");
+                                  }else
+                                  {
+                                    print("Success");
+                                  }
 
-                      ],
-                    ),
-                  ],
+                                },
+
+                                child: Padding(
+                                  padding: const EdgeInsets.fromLTRB(100.0,0,100.0,0),
+                                  child: Text(
+                                    "Register",
+
+
+
+                                  ),
+                                )
+                            ),
+                          )
+
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),

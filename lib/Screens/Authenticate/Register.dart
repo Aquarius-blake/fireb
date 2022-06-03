0133c4d3b5be2efab0e3595bb9311a297096853c
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-03 14:05:53 +0000

    register

--- lib/Screens/Authenticate/Register.dart
+++ lib/Screens/Authenticate/Register.dart
@@ -50,7 +50,7 @@ color: Colors.lightBlueAccent[100],
                     children: <Widget>[
                       SizedBox(height:20 ,),
                       Icon(
-                        LineIcons.forumbee,
+                        LineIcons.newspaper,
                         color: Colors.white70,
                         size: 90,
                       ),

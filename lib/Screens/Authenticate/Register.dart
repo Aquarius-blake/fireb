c317d3640d691b03a27c159bd80af40320714304
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-04 16:43:00 +0000

    Sign in with email and password

--- lib/Screens/Authenticate/Register.dart
+++ lib/Screens/Authenticate/Register.dart
@@ -102,6 +102,10 @@ class _RegisterState extends State<Register> {
                             borderSide: BorderSide(color: Colors.redAccent),
                             borderRadius: BorderRadius.circular(10.0),
                           ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+
+                          ),
                         ),
 
                         style: TextStyle(

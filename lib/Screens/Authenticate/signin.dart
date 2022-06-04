b3261ddb6ea778e9b30ec0dc23f720e808d67839
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-04 15:47:37 +0000

    Sign in with email and password

--- lib/Screens/Authenticate/signin.dart
+++ lib/Screens/Authenticate/signin.dart
@@ -67,7 +67,7 @@ class _SinginState extends State<Singin> {
                   ),
                   SizedBox(height: 20,),
                   TextFormField(
-                    validator: (val)=>val!.isEmpty ? "Please Enter A Valid Email" : null,
+                    validator: (val)=>val!.isEmpty ? "Please Enter A Valid Password" : null,
                     onChanged: (val){
                       setState(() {
                         password=val;

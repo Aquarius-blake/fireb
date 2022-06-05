aa179fe516f787e0631319e311a32430050dbc5f
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-05 16:40:19 +0000

    UI re-design

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -85,6 +85,7 @@ class _HomeState extends State<Home> {
                   },
                 ),
                 SizedBox(height: 20.0,),
+                Divider()
 
               ],
             ),

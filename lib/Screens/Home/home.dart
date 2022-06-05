679b925c4f917e4ac9bd0a7783a4682a433c854e
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-05 16:45:02 +0000

    UI re-design

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -96,6 +96,7 @@ class _HomeState extends State<Home> {
                     style: TextStyle(
                       fontSize: 22.0,
                       fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.italic,
 
                     ),
                   ),

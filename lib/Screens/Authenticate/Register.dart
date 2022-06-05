979239e0c2cad5d1c349719091ad683c558f3546
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-05 16:25:53 +0000

    UI re-design

--- lib/Screens/Authenticate/Register.dart
+++ lib/Screens/Authenticate/Register.dart
@@ -183,10 +183,10 @@ class _RegisterState extends State<Register> {
                           fillColor: Colors.white70,
                           focusedBorder: OutlineInputBorder(
                             borderSide: BorderSide(color: Colors.redAccent),
-                            borderRadius: BorderRadius.circular(10.0),
+                            borderRadius: BorderRadius.circular(100.0),
                           ),
                           enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
+                            borderRadius: BorderRadius.circular(100.0),
 
                           ),
                         ),

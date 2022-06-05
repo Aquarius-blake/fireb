d1dc5da0a7fc239779f7bbcd4619e86a799e143b
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-05 17:05:21 +0000

    UI re-design

--- lib/Screens/Forum/Forum_detail.dart
+++ lib/Screens/Forum/Forum_detail.dart
@@ -1,2 +1,17 @@
 import 'package:flutter/material.dart';
 
+class Forumdetail extends StatefulWidget {
+  const Forumdetail({Key? key}) : super(key: key);
+
+  @override
+  State<Forumdetail> createState() => _ForumdetailState();
+}
+
+class _ForumdetailState extends State<Forumdetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}

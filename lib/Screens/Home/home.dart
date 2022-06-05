88721fd1417ec2616561957fb9797c8204cdf078
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-05 16:48:08 +0000

    UI re-design

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -68,7 +68,7 @@ class _HomeState extends State<Home> {
                 ),
                 SizedBox(height: 40,),
                 ListTile(
-                  leading: Icon(LineIcons.appNet,
+                  leading: Icon(LineIcons.alternateSignOut,
                     color: Colors.black, size:50.0,),
                   title: Text(
                     "Sign Out",

b482a4dde7ddbb7f4d91a00fcf0001e64f04d3c2
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-08 21:00:43 +0000

    UI re-design

--- lib/Models/Users1.dart
+++ lib/Models/Users1.dart
@@ -5,12 +5,12 @@ import 'package:flutter/material.dart';
 class User1{
 
   late final  String UID;
-late final String Email;
-late final String Password;
-late final bool Guest;
-late final Image profilepic;
+late final String? Email;
+late final String? Password;
+late final bool? Guest;
+late final Image? profilepic;
 
 
-  User1({required this.UID});
+  User1({required this.UID, this.profilepic,this.Guest,this.Email,this.Password});
 
 }

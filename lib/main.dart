29c621acb51efc2b1e99712b8d8efc60a2f24f2e
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-08 21:17:28 +0000

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:fire/Services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-
 import 'Models/Users1.dart';
 
 void main() async{

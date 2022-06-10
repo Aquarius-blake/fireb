a882f84becb9acbeec4dee6180869159f0268c79
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-06-10 00:02:51 +0000

    UI Redesign

--- lib/Screens/Home/home.dart
+++ lib/Screens/Home/home.dart
@@ -43,7 +43,7 @@ class _HomeState extends State<Home> {
                       children: <Widget>[
                         CircleAvatar(
                           radius: 60.0,
-                          backgroundImage: AssetImage("Assets/hac.jpg"),
+                          backgroundImage: AssetImage('Assets/hac.jpg'),
                         ),
                         Center(
 
